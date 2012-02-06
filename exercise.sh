e2735e7a751a14d467c6b79c67033f64e1dc8f24
rpedde <ron@pedde.com>
2012-02-06 15:05:16 +0000

    rip out old hander, replace with new

--- exercise.sh
+++ exercise.sh
@@ -1,51 +1,196 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-source ./stackrc
-# Run everything in the exercises/ directory that isn't explicitly disabled
+source ./openrc
 
-# comma separated list of script basenames to skip
-# to refrain from exercising euca.sh use SKIP_EXERCISES=euca
-SKIP_EXERCISES=${SKIP_EXERCISES:-""}
+SKIPPED=0
+FAILED=0
+PASSED=0
 
-# Locate the scripts we should run
-EXERCISE_DIR=$(dirname "$0")/exercises
-basenames=$(for b in `ls $EXERCISE_DIR/*.sh`; do basename $b .sh; done)
+SKIP_MSG=""
 
-# Track the state of each script
-passes=""
-failures=""
-skips=""
+# FIXME: make command-line option override ENV
+PACKAGESET=${PACKAGESET-"diablo-final"}
+BASEDIR=$(dirname $(readlink -f ${0}))
 
-# Loop over each possible script (by basename)
-for script in $basenames; do
-    if [[ "$SKIP_EXERCISES" =~ $script ]] ; then
-        skips="$skips $script"
-    else
-        echo =========================
-        echo Running $script
-        echo =========================
-        $EXERCISE_DIR/$script.sh
-        if [[ $? -ne 0 ]] ; then
-            failures="$failures $script"
-        else
-            passes="$passes $script"
-        fi
+set -u
+
+###
+# set up some globals
+###
+
+black='\033[0m'
+boldblack='\033[1;0m'
+red='\033[31m'
+boldred='\033[1;31m'
+green='\033[32m'
+boldgreen='\033[1;32m'
+yellow='\033[33m'
+boldyellow='\033[1;33m'
+blue='\033[34m'
+boldblue='\033[1;34m'
+magenta='\033[35m'
+boldmagenta='\033[1;35m'
+cyan='\033[36m'
+boldcyan='\033[1;36m'
+white='\033[37m'
+boldwhite='\033[1;37m'
+
+TMPDIR=$(mktemp -d)
+trap "rm -rf ${TMPDIR}" EXIT
+
+
+COLSIZE=40
+
+function should_run() {
+    # $1 - file (nova_api)
+    # $2 - test (list)
+
+    local file=${1}
+    local test=${2}
+    local result=0
+    local expr=""
+    local condition=""
+    local conditions=()
+    local conditions_string=""
+
+    [ -z "${PACKAGESET-}" ] && return 0
+    [ -z "${test_config[${file}]-}" ] || conditions_string=${test_config[${file}]-}
+    [ -z "${test_config[${file-}:${test-}]-}" ] || conditions_string=${test_config[${file-}:${test-}]-}
+    [ -z "${conditions_string-}" ] && return 0
+
+    # side effects rule
+    SKIP_MSG=${conditions_string##*:}
+    conditions_string=${conditions_string%%:*}
+
+    local oldifs="${IFS}"
+    IFS=,
+    conditions=( $conditions_string )
+    IFS="${oldifs}"
+
+    for condition in "${conditions[@]}"; do
+	expr="if [[ ${PACKAGESET} ${condition} ]]; then echo \"yes\"; else echo \"no\"; fi"
+	if [ $(eval ${expr}) == "no" ]; then
+	    result=$(( result + 1 ))
+	fi
+    done
+
+    if [ ${result} -gt 0 ]; then
+	return 1
     fi
+
+    return 0
+}
+
+function colourise() {
+    # $1: colour
+    # $2+ message
+
+    local colour=${1}
+    shift
+    local message="$@"
+
+    if [ -t 1 ] && [ "${TERM}" != "" ]; then
+	eval "printf \"\$${colour}\""
+    fi
+
+    echo ${message}
+
+    if [ -t 1 ] && [ "${TERM}" != "" ]; then
+	tput sgr0
+    fi
+}
+
+
+declare -A test_config
+source testmap.conf
+
+set | grep ' ()' | cut -d' ' -f1 |sort > ${TMPDIR}/fn_pre.txt
+
+echo "Running test suite for packageset \"${PACKAGESET}\""
+
+for d in ${BASEDIR}/exercises/*.sh; do
+    testname=$(basename ${d} .sh)
+
+    source ${d}
+    if $(set | grep -q 'setup ()'); then
+	# not in a subshell, so globals can be modified
+	setup
+    fi
+
+    # find all the functions defined in the newly sourced file.
+    set | grep ' ()' | cut -d' ' -f1 | sort > ${TMPDIR}/fn_post.txt
+    fnlist=$(comm -23 ${TMPDIR}/fn_post.txt ${TMPDIR}/fn_pre.txt)
+    echo -e "\n=== ${testname} ===\n"
+
+    # run each test
+    for test in ${fnlist}; do
+	[[ ${test} =~ "setup" ]] && continue
+	[[ ${test} =~ "teardown" ]] && continue
+
+    	printf " %-${COLSIZE}s" "${test}"
+	SKIP_MSG=""
+
+	if should_run ${testname} ${test}; then
+	    resultcolour="green"  # for you, darren :p
+	    start=$(date +%s.%N)
+
+	    echo "=== TEST: ${testname}/${test} ===" > ${TMPDIR}/test.txt
+
+	    eval "(set -e; set -x; ${test}; set +x; set +e); status=\$?" >> ${TMPDIR}/test.txt 2>&1
+
+	    end=$(date +%s.%N)
+
+	    elapsed=$(echo "${end}-${start}*100/100" | bc -q 2> /dev/null)
+	    result="OK"
+	    if [ ${status} -ne 0 ]; then
+		resultcolour="red"
+		result="FAIL"
+		cat ${TMPDIR}/test.txt >> ${TMPDIR}/notice.txt
+		echo >> ${TMPDIR}/notice.txt
+
+		FAILED=$(( ${FAILED} + 1 ))
+	    else
+		result=$(printf "%0.3fs" "${elapsed}")
+		PASSED=$(( ${PASSED} + 1 ))
+	    fi
+
+	    colourise ${resultcolour} " ${result}"
+	else
+	    colourise boldyellow -n " SKIP"
+	    if [ ! -z "${SKIP_MSG-}" ]; then
+		echo ": ${SKIP_MSG}"
+	    else
+		echo
+	    fi
+
+	    SKIPPED=$(( ${SKIPPED} + 1 ))
+	fi
+    done
+
+    if $(set | grep -q 'teardown ()'); then
+	teardown
+    fi
+
+    # undefine the tests
+    for test in ${fnlist}; do
+	unset -f ${test}
+    done
 done
 
-# output status of exercise run
-echo =========================
-echo =========================
-for script in $skips; do
-    echo SKIP $script
-done
-for script in $passes; do
-    echo PASS $script
-done
-for script in $failures; do
-    echo FAILED $script
-done
+echo
+echo "RESULTS:"
 
-if [ -n "$failures" ] ; then
+echo -n "Passed:  "
+colourise green ${PASSED}
+echo -n "Failed:  "
+colourise red ${FAILED}
+echo -n "Skipped: "
+colourise boldyellow ${SKIPPED}
+
+echo
+if [ "$FAILED" -ne "0" ]; then
+    colourise red ERROR TEST OUTPUT
+    cat ${TMPDIR}/notice.txt
     exit 1
 fi
+
--- exercises/bundle.sh
+++ exercises/bundle.sh
@@ -1,48 +1,35 @@
-#!/usr/bin/env bash
+function setup() {
+    # Remove old certificates
+    rm -f cacert.pem
+    rm -f cert.pem
+    rm -f pk.pem
+}
 
-# we will use the ``euca2ools`` cli tool that wraps the python boto
-# library to test ec2 compatibility
-#
+function 001_get_certificates() {
+    # Get Certificates
+    nova x509-get-root-cert
+    nova x509-create-cert
+}
 
-# This script exits on an error so that errors don't compound and you see
-# only the first error that occured.
-set -o errexit
+function 002_register_bundle() {
+    # Max time to wait for image to be registered
+    REGISTER_TIMEOUT=${REGISTER_TIMEOUT:-15}
 
-# Print the commands being run so that we can see the command that triggers
-# an error.  It is also useful for following allowing as the install occurs.
-set -o xtrace
+    BUCKET=testbucket
+    IMAGE=bundle.img
+    truncate -s 5M ${TMPDIR}/$IMAGE
+    euca-bundle-image -i ${TMPDIR}/$IMAGE
 
-# Settings
-# ========
-
-# Use openrc + stackrc + localrc for settings
-pushd $(cd $(dirname "$0")/.. && pwd)
-source ./openrc
-
-# Remove old certificates
-rm -f cacert.pem
-rm -f cert.pem
-rm -f pk.pem
-
-# Get Certificates
-nova x509-get-root-cert
-nova x509-create-cert
-popd
-
-# Max time to wait for image to be registered
-REGISTER_TIMEOUT=${REGISTER_TIMEOUT:-15}
-
-BUCKET=testbucket
-IMAGE=bundle.img
-truncate -s 5M /tmp/$IMAGE
-euca-bundle-image -i /tmp/$IMAGE
+    euca-upload-bundle -b $BUCKET -m ${TMPDIR}/$IMAGE.manifest.xml
 
 
-euca-upload-bundle -b $BUCKET -m /tmp/$IMAGE.manifest.xml
-AMI=`euca-register $BUCKET/$IMAGE.manifest.xml | cut -f2`
+    AMI=`euca-register $BUCKET/$IMAGE.manifest.xml | cut -f2`
 
-# Wait for the image to become available
-if ! timeout $REGISTER_TIMEOUT sh -c "while euca-describe-images | grep '$AMI' | grep 'available'; do sleep 1; done"; then
-    echo "Image $AMI not available within $REGISTER_TIMEOUT seconds"
-    exit 1
-fi
+    # Wait for the image to become available
+    if ! timeout $REGISTER_TIMEOUT sh -c "while euca-describe-images | grep '$AMI' | grep 'available'; do sleep 1; done"; then
+        echo "Image $AMI not available within $REGISTER_TIMEOUT seconds"
+        exit 1
+    fi
+}
+
+
