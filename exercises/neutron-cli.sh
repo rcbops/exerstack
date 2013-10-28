a1554b40520033fa55b83f9c28fd1f7f63afbe6a
invsblduck <invsblduck@gmail.com>
2013-10-28 10:15:05 -0700

    pls stop shouting and breaking gating kthx

--- exercises/neutron-cli.sh
+++ exercises/neutron-cli.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+NEUTRON_BIN=neutron
+if [ "${0##*/}" = "quantum-cli.sh" ]; then
+    NEUTRON_BIN=quantum
+fi
+
 function setup() {
-export NETWORK_NAME='exerstack_test_network'
-export NETWORK_NAME2='a_new_network_name'
-export SUBNET_NAME='exerstack_test_subnet'
-export SUBNET_NAME2='a_new_subnet_name'
-export SUBNET_CIDR='192.168.57.0/24'
-export PORT_NAME='exerstack_test_port'
-export PORT_NAME2='a_new_port_name'
-export Q_SECGROUP_NAME='exerstack_test_security_group'
+    export NETWORK_NAME='exerstack_test_network'
+    export NETWORK_NAME2='a_new_network_name'
+    export SUBNET_NAME='exerstack_test_subnet'
+    export SUBNET_NAME2='a_new_subnet_name'
+    export SUBNET_CIDR='192.168.57.0/24'
+    export PORT_NAME='exerstack_test_port'
+    export PORT_NAME2='a_new_port_name'
+    export Q_SECGROUP_NAME='exerstack_test_security_group'
 }
 
 #  agent-delete                   Delete a given agent.
@@ -78,15 +83,15 @@ export Q_SECGROUP_NAME='exerstack_test_security_group'
 ####  subnet-update                  Update subnet's information.
 
 function 010_agent-list() {
-    if ! neutron agent-list; then
-        echo "could not list neutron agents"
+    if ! ${NEUTRON_BIN} agent-list; then
+        echo "could not list ${NEUTRON_BIN} agents"
         return 1
     fi
 }
 
 function 020_agent-show() {
-    AGENT_ID=$(neutron agent-list -f csv | tail -n 1 | cut -d '"' -f2)
-    if ! neutron agent-show ${AGENT_ID}; then
+    AGENT_ID=$(${NEUTRON_BIN} agent-list -f csv | tail -n 1 | cut -d '"' -f2)
+    if ! ${NEUTRON_BIN} agent-show ${AGENT_ID}; then
         echo "could not get detailed information about agent ${AGENT_ID}"
         return 1
     fi
@@ -94,29 +99,29 @@ function 020_agent-show() {
 }
 
 function 025_ext-list() {
-    if ! neutron ext-list; then
-        echo "could not list neutron extensions"
+    if ! ${NEUTRON_BIN} ext-list; then
+        echo "could not list ${NEUTRON_BIN} extensions"
         return 1
     fi
 }
 
 function 028_ext-show() {
-    if ! neutron ext-show 'agent'; then
+    if ! ${NEUTRON_BIN} ext-show 'agent'; then
         echo "could not get detailed information about agent extension "
         return 1
     fi
 }
 
 function 030_net-list() {
-    if ! neutron net-list; then
+    if ! ${NEUTRON_BIN} net-list; then
         echo "could not list networks"
         return 1
     fi
 }
 
 function 040_net-list-on-dhcp-agent() {
-    if DHCP_AGENT_ID=$(neutron agent-list -f csv | grep -i dhcp | tail -1 | cut -d'"' -f2); then
-        if ! neutron net-list-on-dhcp-agent ${DHCP_AGENT_ID}; then
+    if DHCP_AGENT_ID=$(${NEUTRON_BIN} agent-list -f csv | grep -i dhcp | tail -1 | cut -d'"' -f2); then
+        if ! ${NEUTRON_BIN} net-list-on-dhcp-agent ${DHCP_AGENT_ID}; then
             echo "could not list networks on dhcp agent ${DHCP_AGENT_ID}"
             return 1
         fi
@@ -128,20 +133,20 @@ function 040_net-list-on-dhcp-agent() {
 }
 
 function 050_net-create() {
-    if ! neutron net-create  ${NETWORK_NAME}; then
+    if ! ${NEUTRON_BIN} net-create  ${NETWORK_NAME}; then
         echo "could not create network"
         return 1
     fi
 
-    if ! neutron net-list | grep ${NETWORK_NAME}; then
+    if ! ${NEUTRON_BIN} net-list | grep ${NETWORK_NAME}; then
         echo "subnet was added, but does not show in subnet list output"
         return 1
     fi
 }
 
 function 060_net-show() {
-    NETWORK_ID=$(neutron net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
-    if ! neutron net-show  ${NETWORK_ID}; then
+    NETWORK_ID=$(${NEUTRON_BIN} net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} net-show  ${NETWORK_ID}; then
         echo "could not show network"
         return 1
     fi
@@ -149,32 +154,32 @@ function 060_net-show() {
 }
 
 function 070_net-update() {
-    NETWORK_ID=$(neutron net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
-    if ! neutron net-update ${NETWORK_ID} --name ${NETWORK_NAME2}; then
+    NETWORK_ID=$(${NEUTRON_BIN} net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} net-update ${NETWORK_ID} --name ${NETWORK_NAME2}; then
         echo "could not update network name"
         return 1
     fi
 
-    if ! [ "${NETWORK_NAME2}" = "$(neutron net-show -f shell ${NETWORK_ID} | grep '^name=' | cut -d'"' -f2)" ]; then
+    if ! [ "${NETWORK_NAME2}" = "$(${NEUTRON_BIN} net-show -f shell ${NETWORK_ID} | grep '^name=' | cut -d'"' -f2)" ]; then
         echo "network was not updated properly"
         return 1
     fi
 
-    neutron net-update ${NETWORK_ID} --name ${NETWORK_NAME}
+    ${NEUTRON_BIN} net-update ${NETWORK_ID} --name ${NETWORK_NAME}
 }
 
 function 080_dhcp-agent-network-add() {
-    if DHCP_AGENT_ID=$(neutron agent-list -f csv |grep -i dhcp | tail -1 | cut -d'"' -f2); then
-        NETWORK_ID=$(neutron net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
-        if ! neutron dhcp-agent-network-add ${DHCP_AGENT_ID} ${NETWORK_ID}; then
+    if DHCP_AGENT_ID=$(${NEUTRON_BIN} agent-list -f csv |grep -i dhcp | tail -1 | cut -d'"' -f2); then
+        NETWORK_ID=$(${NEUTRON_BIN} net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
+        if ! ${NEUTRON_BIN} dhcp-agent-network-add ${DHCP_AGENT_ID} ${NETWORK_ID}; then
             echo "could not add network ${NETWORK_ID} to dhcp agent ${DHCP_AGENT_ID}"
             return 1
         fi
-        if ! neutron net-list-on-dhcp-agent ${DHCP_AGENT_ID} | grep ${NETWORK_ID}; then
+        if ! ${NEUTRON_BIN} net-list-on-dhcp-agent ${DHCP_AGENT_ID} | grep ${NETWORK_ID}; then
             echo "network was added to dhcp agent, but does not show in it's listing"
             return 1
         fi
-        if ! neutron dhcp-agent-list-hosting-net ${NETWORK_ID} | grep ${DHCP_AGENT_ID}; then
+        if ! ${NEUTRON_BIN} dhcp-agent-list-hosting-net ${NETWORK_ID} | grep ${DHCP_AGENT_ID}; then
             echo "network was added to dhcp agent, but does not show in list of agents hosting this network"
         return 1
         fi
@@ -187,84 +192,84 @@ function 080_dhcp-agent-network-add() {
 }
 
 function 090_subnet-create() {
-    NETWORK_ID=$(neutron net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
+    NETWORK_ID=$(${NEUTRON_BIN} net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
 
-    if ! neutron subnet-create --name ${SUBNET_NAME} ${NETWORK_ID} ${SUBNET_CIDR}; then
+    if ! ${NEUTRON_BIN} subnet-create --name ${SUBNET_NAME} ${NETWORK_ID} ${SUBNET_CIDR}; then
         echo "could not create subnet"
         return 1
     fi
 
-    if ! neutron subnet-list | grep ${SUBNET_NAME}; then
+    if ! ${NEUTRON_BIN} subnet-list | grep ${SUBNET_NAME}; then
         echo "subnet was added, but does not show in subnet list output"
         return 1
     fi
 }
 
 function 100_subnet-show() {
-    SUBNET_ID=$(neutron subnet-show ${SUBNET_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
-    if ! neutron subnet-show  ${SUBNET_ID}; then
+    SUBNET_ID=$(${NEUTRON_BIN} subnet-show ${SUBNET_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} subnet-show  ${SUBNET_ID}; then
         echo "could not show subnet"
         return 1
     fi
 }
 
 function 110_subnet-update() {
-    SUBNET_ID=$(neutron subnet-show ${SUBNET_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
-    if ! neutron subnet-update ${SUBNET_ID} --name ${SUBNET_NAME2}; then
+    SUBNET_ID=$(${NEUTRON_BIN} subnet-show ${SUBNET_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} subnet-update ${SUBNET_ID} --name ${SUBNET_NAME2}; then
         echo "could not update subnet name"
         return 1
     fi
 
-    if ! [ "${SUBNET_NAME2}" = "$(neutron subnet-show -f shell ${SUBNET_ID} | grep '^name=' | cut -d'"' -f2)" ]; then
+    if ! [ "${SUBNET_NAME2}" = "$(${NEUTRON_BIN} subnet-show -f shell ${SUBNET_ID} | grep '^name=' | cut -d'"' -f2)" ]; then
         echo "subnet was not updated properly"
         return 1
     fi
 
-    neutron subnet-update ${SUBNET_ID} --name ${SUBNET_NAME}
+    ${NEUTRON_BIN} subnet-update ${SUBNET_ID} --name ${SUBNET_NAME}
 }
 
 function 120_port-create() {
-    NETWORK_ID=$(neutron net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
+    NETWORK_ID=$(${NEUTRON_BIN} net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
 
-    if ! neutron port-create --name ${PORT_NAME} ${NETWORK_ID}; then
+    if ! ${NEUTRON_BIN} port-create --name ${PORT_NAME} ${NETWORK_ID}; then
         echo "could not create port"
         return 1
     fi
 
-    if ! neutron port-list | grep ${PORT_NAME}; then
+    if ! ${NEUTRON_BIN} port-list | grep ${PORT_NAME}; then
         echo "port was added, but does not show in port list output"
         return 1
     fi
 }
 
 function 130_port-show() {
-    PORT_ID=$(neutron port-show ${PORT_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
-    if ! neutron port-show  ${PORT_ID}; then
+    PORT_ID=$(${NEUTRON_BIN} port-show ${PORT_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} port-show  ${PORT_ID}; then
         echo "could not show port"
         return 1
     fi
 }
 
 function 140_port-update() {
-    PORT_ID=$(neutron port-show ${PORT_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
-    if ! neutron port-update ${PORT_ID} --name ${PORT_NAME2}; then
+    PORT_ID=$(${NEUTRON_BIN} port-show ${PORT_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} port-update ${PORT_ID} --name ${PORT_NAME2}; then
         echo "could not update port name"
         return 1
     fi
 
-    if ! [ "${PORT_NAME2}" = "$(neutron port-show -f shell ${PORT_ID} | grep '^name=' | cut -d'"' -f2)" ]; then
+    if ! [ "${PORT_NAME2}" = "$(${NEUTRON_BIN} port-show -f shell ${PORT_ID} | grep '^name=' | cut -d'"' -f2)" ]; then
         echo "port was not updated properly"
         return 1
     fi
 
-    neutron port-update ${PORT_ID} --name ${PORT_NAME}
+    ${NEUTRON_BIN} port-update ${PORT_ID} --name ${PORT_NAME}
 }
 
 function 150_quota-update() {
-    CURRENT_SUBNET_QUOTA=$(neutron quota-show -f shell | grep '^subnet=' | cut -d'"' -f2)
+    CURRENT_SUBNET_QUOTA=$(${NEUTRON_BIN} quota-show -f shell | grep '^subnet=' | cut -d'"' -f2)
     TARGET_SUBNET_QUOTA=$(( CURRENT_SUBNET_QUOTA +1 ))
-    neutron quota-update --subnet ${TARGET_SUBNET_QUOTA}
-    NEW_SUBNET_QUOTA=$(neutron quota-show -f shell |  grep '^subnet=' | cut -d'"' -f2)
+    ${NEUTRON_BIN} quota-update --subnet ${TARGET_SUBNET_QUOTA}
+    NEW_SUBNET_QUOTA=$(${NEUTRON_BIN} quota-show -f shell |  grep '^subnet=' | cut -d'"' -f2)
 
     if [ ${NEW_SUBNET_QUOTA} != ${TARGET_SUBNET_QUOTA} ]; then
         echo "could not update quotas for tenant"
@@ -274,118 +279,118 @@ function 150_quota-update() {
 
 function 160_quota-list() {
     # separate quota for this tenant should now exist after changing a value above
-    if ! neutron quota-list ; then
+    if ! ${NEUTRON_BIN} quota-list ; then
         echo "could not list quotas for tenants"
         return 1
     fi
 }
 
 function 170_quota-show() {
-    if ! neutron quota-show; then
+    if ! ${NEUTRON_BIN} quota-show; then
         echo "could not show quotas for this tenant"
         return 1
     fi
 }
 
 function 180_security-group-create() {
-    if ! neutron security-group-create $Q_SECGROUP_NAME; then
+    if ! ${NEUTRON_BIN} security-group-create $Q_SECGROUP_NAME; then
         echo "could not create security group"
         return 1
     fi
-    if ! neutron security-group-list | grep ${Q_SECGROUP_NAME}; then
+    if ! ${NEUTRON_BIN} security-group-list | grep ${Q_SECGROUP_NAME}; then
         echo "security group was created but does not show in list output"
         return 1
     fi
 }
 
 function 190_security-group-show() {
-    if ! neutron security-group-show ${Q_SECGROUP_NAME}; then
+    if ! ${NEUTRON_BIN} security-group-show ${Q_SECGROUP_NAME}; then
         echo "could not show details of security group ${Q_SECGROUP_NAME}"
         return 1
     fi
 }
 
 function 200_security-group-rule-create() {
-    if ! neutron security-group-rule-create --protocol icmp ${Q_SECGROUP_NAME}; then
+    if ! ${NEUTRON_BIN} security-group-rule-create --protocol icmp ${Q_SECGROUP_NAME}; then
         echo "could not create security group rule"
         return 1
     fi
-    if ! neutron security-group-rule-list | grep ${Q_SECGROUP_NAME} | grep icmp; then
+    if ! ${NEUTRON_BIN} security-group-rule-list | grep ${Q_SECGROUP_NAME} | grep icmp; then
         echo "created security group rule but can't see it in list output"
         return 1
     fi
 }
 
 function 210_security-group-rule-show() {
-    Q_SECGROUP_RULE_ID=$(neutron security-group-rule-list -f csv | grep ${Q_SECGROUP_NAME} | grep icmp | cut -d'"' -f2)
-    if ! neutron security-group-rule-show ${Q_SECGROUP_RULE_ID}; then
+    Q_SECGROUP_RULE_ID=$(${NEUTRON_BIN} security-group-rule-list -f csv | grep ${Q_SECGROUP_NAME} | grep icmp | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} security-group-rule-show ${Q_SECGROUP_RULE_ID}; then
         echo "could not show security group rule details"
         return 1
     fi
 }
 
 function 220_security-group-rule-delete() {
-    Q_SECGROUP_RULE_ID=$(neutron security-group-rule-list -f csv | grep ${Q_SECGROUP_NAME} | grep icmp | cut -d'"' -f2)
-    if ! neutron security-group-rule-delete ${Q_SECGROUP_RULE_ID}; then
+    Q_SECGROUP_RULE_ID=$(${NEUTRON_BIN} security-group-rule-list -f csv | grep ${Q_SECGROUP_NAME} | grep icmp | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} security-group-rule-delete ${Q_SECGROUP_RULE_ID}; then
         echo "could not delete security group rule"
         return 1
     fi
 
-    if neutron security-group-rule-show ${Q_SECGROUP_RULE_ID}; then
+    if ${NEUTRON_BIN} security-group-rule-show ${Q_SECGROUP_RULE_ID}; then
         echo "security group rule was deleted but still shows in output"
         return 1
     fi
 }
 
 function 300_security-group-delete() {
-    if ! neutron security-group-delete ${Q_SECGROUP_NAME}; then
+    if ! ${NEUTRON_BIN} security-group-delete ${Q_SECGROUP_NAME}; then
         echo "could not delete security group rule"
         return 1
     fi
 
-    if neutron security-group--show ${Q_SECGROUP_NAME}; then
+    if ${NEUTRON_BIN} security-group--show ${Q_SECGROUP_NAME}; then
         echo "security group was deleted but still shows in output"
         return 1
     fi
 }
 
 function 310_quota-delete() {
-    if ! neutron quota-delete; then
+    if ! ${NEUTRON_BIN} quota-delete; then
         echo "could not delete the quotas for this tenant"
         return 1
     fi
-    if neutron quota-list | grep 'subnet'; then
+    if ${NEUTRON_BIN} quota-list | grep 'subnet'; then
         echo "quota was deleted, but is still showing in in list output"
         return 1
     fi
 }
 
 function 320_port-delete() {
-    PORT_ID=$(neutron port-show ${PORT_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
-    if ! neutron port-delete ${PORT_ID}; then
+    PORT_ID=$(${NEUTRON_BIN} port-show ${PORT_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} port-delete ${PORT_ID}; then
         echo "could not delete port ${PORT_ID}"
         return 1
     fi
 }
 
 function 330_subnet-delete() {
-    SUBNET_ID=$(neutron subnet-show ${SUBNET_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
-    if ! neutron subnet-delete ${SUBNET_ID}; then
+    SUBNET_ID=$(${NEUTRON_BIN} subnet-show ${SUBNET_NAME} -f shell | grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} subnet-delete ${SUBNET_ID}; then
         echo "could not delete subnet"
         return 1
     fi
 }
 
 function 340_dhcp-agent-network-remove() {
-    if DHCP_AGENT_ID=$(neutron agent-list -f csv |grep -i dhcp | tail -1 | cut -d'"' -f2); then
-        NETWORK_ID=$(neutron net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
-        if ! neutron dhcp-agent-network-remove ${DHCP_AGENT_ID} ${NETWORK_ID}; then
+    if DHCP_AGENT_ID=$(${NEUTRON_BIN} agent-list -f csv |grep -i dhcp | tail -1 | cut -d'"' -f2); then
+        NETWORK_ID=$(${NEUTRON_BIN} net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
+        if ! ${NEUTRON_BIN} dhcp-agent-network-remove ${DHCP_AGENT_ID} ${NETWORK_ID}; then
             echo "could not remove network from dhcp agent"
         fi
-        if neutron dhcp-agent-list-hosting-net ${NETWORK_ID} | grep ${DHCP_AGENT_ID}; then
+        if ${NEUTRON_BIN} dhcp-agent-list-hosting-net ${NETWORK_ID} | grep ${DHCP_AGENT_ID}; then
             echo "network was removed from dhcp agent but still shows in network's listing"
         fi
-        if neutron net-list-on-dhcp-agent ${DHCP_AGENT_ID} | grep ${NETWORK_ID}; then
+        if ${NEUTRON_BIN} net-list-on-dhcp-agent ${DHCP_AGENT_ID} | grep ${NETWORK_ID}; then
             echo "network was removed from dhcp agent but still shows in agent'slisting"
         fi
     else
@@ -396,8 +401,8 @@ function 340_dhcp-agent-network-remove() {
 }
 
 function 350_net-delete() {
-    NETWORK_ID=$(neutron net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
-    if ! neutron net-delete  ${NETWORK_ID}; then
+    NETWORK_ID=$(${NEUTRON_BIN} net-show ${NETWORK_NAME} -f shell |grep '^id=' | cut -d'"' -f2)
+    if ! ${NEUTRON_BIN} net-delete  ${NETWORK_ID}; then
         echo "could not delete network"
         return 1
     fi
